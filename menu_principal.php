6e4406c1b2bfc31988b588d456f21f62b488dc94
OKARLES <louecharles35@gmail.com>
2020-10-24 13:26:33 +0100

    Nouvelle version de poupouche

---
+++ menu_principal.php
@@ -0,0 +1,11 @@
+<nav id="menu" class="menu">
+                                        <ul class="dropdown">
+                                           <li class="active"><a href="index.php">Acceuil</a>
+                                                
+                                            </li>
+                                            <li><a href="apropos.php">A propos</a></li>
+                                            <li ><a href="nouscontacter.php">Nous contacter</a></li>
+                                            <li ><a href="connexion.php">Connexion</a></li>
+                                            
+                                        </ul>
+                                    </nav>
